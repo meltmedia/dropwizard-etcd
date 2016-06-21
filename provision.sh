a5c2ccc1f07a1f175a7f4fe07a71f25c539d6d45
Quinn Thibeault <quinn.thibeault@meltmedia.com>
2016-06-21 13:57:38 -0700

    Move etcd version to 2.3.7

--- provision.sh
+++ provision.sh
@@ -19,8 +19,8 @@
 sudo apt-get update
 
 # install etcd from a github tag.
-sudo mkdir /opt/etcd-v2.0.10-linux-amd64
-curl -Ls https://github.com/coreos/etcd/releases/download/v2.0.10/etcd-v2.0.10-linux-amd64.tar.gz | sudo tar xzv --strip-components=1 -C /opt/etcd-v2.0.10-linux-amd64
+sudo mkdir /opt/etcd-v2.3.7-linux-amd64
+curl -sL https://github.com/coreos/etcd/releases/download/v2.3.7/etcd-v2.3.7-linux-amd64.tar.gz | sudo tar xzv --strip-components=1 -C /opt/etcd-v2.3.7-linux-amd64
 
 sudo cp /vagrant/etc/etcd.conf /etc/init
 sudo service etcd start
